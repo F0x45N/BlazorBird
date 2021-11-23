a96ff3d7ffd3bd573c22304d04b6967af95384e0
Yotsawang Sorangkoun <yotsawangs@gmail.com>
2021-11-23 21:36:47 +0700

    Project Setup

---
+++ BlazorBird/ClassModels/BirdModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorBird.ClassModels
+{
+    public class BirdModel
+    {
+        public int DistanceFromGround { get; set; } = 100;
+
+        public void Fall(int gravity)
+        {
+            DistanceFromGround -= gravity;
+        }
+
+    }
+}
